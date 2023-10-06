75229485c28650b40fe54f526c0c3538b0014aa1
richasudikonda <vermaricha829@gmail.com>
2023-10-06 11:30:13 -0400

    Header Title

---
+++ src/component/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header className="header">
+            <h2 className="header--title">Auromated QR Generator </h2>
+        </header>
+    )
+}
